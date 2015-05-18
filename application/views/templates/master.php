31076cd575ecd6fc7a175f0977706cb680c4e9cd
Amy Martin <akm96@uclive.ac.nz>
2015-05-18 12:52:08 +1200

    added authentication config and background color

--- application/views/templates/master.php
+++ application/views/templates/master.php
@@ -3,7 +3,7 @@ $URL_SPLIT = '://';
 $url_pieces = explode($URL_SPLIT, base_url());
 $logout_url = $url_pieces[0] . $URL_SPLIT .'logout@' . $url_pieces[1];
 
-echo doctype('html5');  
+echo doctype('html5');
 ?>
 
 <html>
@@ -14,7 +14,7 @@ echo doctype('html5');
         <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">-->
         <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">-->
         <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>-->
-        <link href="<?php echo base_url('assets/css/base.css'); ?>" rel="stylesheet" type="text/css" /> 
+        <link href="<?php echo base_url('assets/css/base.css'); ?>" rel="stylesheet" type="text/css" />
         <link rel="icon" href="<?php echo base_url('assets/img/favicon.ico')?>" type="image/ico"/>
     </head>
     <body>
@@ -22,6 +22,6 @@ echo doctype('html5');
         <p class='logout'><a href="<?php echo $logout_url ?>" >logout</a></p>
         <?php echo $content; ?>
         <hr>
-        <p style="font-weight:bold">&copy; Amy Martin 2015</p>
+        <p style="font-weight:bold">2015</p>
     </body>
 </html>
